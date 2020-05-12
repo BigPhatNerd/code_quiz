ed4c628192ec330cd9a798a63e842c4b7fa2804a
Wilson <wilsonhorrell@git.training>
2020-05-12 16:21:58 -0500

    Finished quiz challenge

--- script.js
+++ script.js
@@ -1,1 +1,93 @@
-console.log('beyatch');
+var quizContainer = document.querySelector("#quiz");
+var resultsContainer = document.querySelector("#results");
+var submitButton = document.querySelector("#submit");
+var myQuestions = [{
+        question: "What is 10/2?",
+        answers: {
+            a: "3",
+            b: "5",
+            c: "115"
+        },
+        correctAnswer: 'b'
+    },
+    {
+        question: "What is 30/3?",
+        answers: {
+            a: "3",
+            b: "5",
+            c: "10"
+        },
+        correctAnswer: 'c'
+    }
+];
+
+
+
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+
+    function showQuestions(questions, quizContainer) {
+        var output = [];
+        var answers;
+
+        for (var i = 0; i < questions.length; i++) {
+            answers = [];
+            for (letter in questions[i].answers) {
+                answers.push(
+                    '<label>' +
+                    '<input type="radio" name="question' + i + '" value="' + letter + '">' +
+                    letter + ': ' +
+                    questions[i].answers[letter] +
+                    '</label>'
+                );
+            }
+            output.push(
+                '<div class="question">' + questions[i].question + '<div>' +
+                '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+        quizContainer.innerHTML = output.join('');
+
+    }
+
+    function showResults(myQuestions, quizContainer, resultsContainer) {
+
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+
+        // for each question...
+        for (var i = 0; i < questions.length; i++) {
+
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+
+            // if answer is correct
+            if (userAnswer === questions[i].correctAnswer) {
+                // add to the number of correct answers
+                numCorrect++;
+
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else {
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+    submitButton.onclick = function() {
+
+        showResults(myQuestions, quizContainer, resultsContainer);
+    }
+    showQuestions(myQuestions, quizContainer);
+}
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton)
